239dfb6b75692c58b7af111fa7cbd65c030536fe
Alexander Hagmann <ahagmann@css.edu>
2019-11-03 21:45:28 -0600

    Practice commit to see if it works.

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
+//Alex Hagmann
 namespace SingleResponsibilityPrinciple
 {
     public class TradeProcessor
